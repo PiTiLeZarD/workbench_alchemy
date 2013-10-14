d3e64b3c3f07b1e1e7d6099f82f2890d73ad46e6
Jonathan Adami <pitilezard@gmail.com>
2013-10-14 16:42:11 +1100

    pep8 compat

--- sqlalchemy_grt.py
+++ sqlalchemy_grt.py
@@ -1,6 +1,6 @@
 import re
 
-version = '0.10.1'
+version = '0.10.2'
 
 types = {
     'sqla': [],
@@ -20,18 +20,25 @@ sqlalchemy_typesmap = {
 }
 
 USE_MYSQL_TYPES = True
-mysqltypes = ['BIGINT', 'BINARY', 'BIT', 'BLOB', 'BOOLEAN', 'CHAR', 'DATE', 'DATETIME', 'DECIMAL', 'DECIMAL', 'DOUBLE', 'ENUM', 'FLOAT', 'INTEGER', 'LONGBLOB', 'LONGTEXT', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'NCHAR', 'NUMERIC', 'NVARCHAR', 'REAL', 'SET', 'SMALLINT', 'TEXT', 'TIME', 'TIMESTAMP', 'TINYBLOB', 'TINYINT', 'TINYTEXT', 'VARBINARY', 'VARCHAR', 'YEAR']
+mysqltypes = [
+    'BIGINT', 'BINARY', 'BIT', 'BLOB', 'BOOLEAN', 'CHAR', 'DATE', 'DATETIME', 'DECIMAL', 'DECIMAL', 'DOUBLE', 'ENUM', 'FLOAT', 'INTEGER',
+    'LONGBLOB', 'LONGTEXT', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'NCHAR', 'NUMERIC', 'NVARCHAR', 'REAL', 'SET', 'SMALLINT', 'TEXT',
+    'TIME', 'TIMESTAMP', 'TINYBLOB', 'TINYINT', 'TINYTEXT', 'VARBINARY', 'VARCHAR', 'YEAR']
 
-def camelize( name ):
+
+def camelize(name):
     return re.sub(r"(?:^|_)(.)", lambda x: x.group(0)[-1].upper(), name)
 
-def endsWith( name, all ):
+
+def endsWith(name, all):
     name = name.lower()
     for i in all:
-        if name.endswith(i): return True
+        if name.endswith(i):
+            return True
     return False
 
-def singular( name ):
+
+def singular(name):
     if endsWith(name, ('indices',)):
         name = name[:-4] + 'ex'
     elif endsWith(name, ('suffixes',)):
@@ -44,7 +51,8 @@ def singular( name ):
         name = name[:-1]
     return name
 
-def getType( column ):
+
+def getType(column):
     column_type = column.formattedType
     column_type = re.match(r'(?P<type>[^\(\)]+)(\((?P<size>[^\(\)]+)\))?', column_type).groupdict()
     column_type, size = (column_type['type'], column_type['size'])
@@ -53,10 +61,10 @@ def getType( column ):
     if USE_MYSQL_TYPES and column_type in mysqltypes:
         # case of mysql types
         column_type = column_type.upper()
-        if column_type not in types['mysql']: 
+        if column_type not in types['mysql']:
             types['mysql'].append(column_type)
-            
-            sqla = camelize( column_type.lower() )
+
+            sqla = camelize(column_type.lower())
             sqla = sqlalchemy_typesmap.get(sqla, sqla)
             types['sqla_alt'].append("%s as %s" % (sqla, column_type))
 
@@ -65,16 +73,18 @@ def getType( column ):
 
     else:
         # sqlalchemy types
-        column_type = camelize( column_type.lower() )
+        column_type = camelize(column_type.lower())
         column_type = sqlalchemy_typesmap.get(column_type, column_type)
-        if column_type not in types['sqla']: types['sqla'].append(column_type)
+        if column_type not in types['sqla']:
+            types['sqla'].append(column_type)
 
     if size and 'INT' not in column_type.upper():
         column_type = '%s(%s)' % (column_type, size)
 
     return column_type
 
-def exportTable( table ):
+
+def exportTable(table):
     export = []
 
     # yeah I know... but I can't prevent myself...
@@ -82,8 +92,8 @@ def exportTable( table ):
     # to a dict like {column_name: {opt1:value1, opt2:value2} ...}
     options = dict([(c.name, dict([t.split('=') for t in (c.comment or '').split(',') if '=' in t])) for c in table.columns])
 
-    classname = singular( camelize( table.name ) )
-    indices = {'PRIMARY':[], 'INDEX':[], 'UNIQUE':[]}
+    classname = singular(camelize(table.name))
+    indices = {'PRIMARY': [], 'INDEX': [], 'UNIQUE': []}
 
     for index in table.indices:
         if index.indexType == 'PRIMARY':
@@ -112,15 +122,15 @@ def exportTable( table ):
     inherits = 'Base'
     if 'abstract' in table.comment:
         inherits = 'object'
+    export.append("")
     export.append("class %s(%s):" % (classname, inherits))
     if 'abstract' not in table.comment:
-        export.append("  __tablename__ = '%s'" % table.name)
-        export.append("  ")
+        export.append("    __tablename__ = '%s'" % table.name)
+        export.append("")
 
     aliases = {}
     for column in table.columns:
         column_name = column.name
-        column_alias = ''
         column_type = getType(column)
 
         if 'alias' in options[column_name]:
@@ -132,8 +142,10 @@ def exportTable( table ):
         if column.name in foreignKeys:
             fkcol, fktable, ondelete, onupdate = foreignKeys[column.name]
             fkopts = []
-            if ondelete: fkopts.append('ondelete="%s"' % ondelete)
-            if onupdate: fkopts.append('onupdate="%s"' % onupdate)
+            if ondelete:
+                fkopts.append('ondelete="%s"' % ondelete)
+            if onupdate:
+                fkopts.append('onupdate="%s"' % onupdate)
             fkopts = len(fkopts) and ', ' + ', '.join(fkopts) or ''
             column_options.append('ForeignKey("%s"%s)' % (fkcol, fkopts))
         if column.isNotNull == 1:
@@ -155,15 +167,16 @@ def exportTable( table ):
             column_options = ['"%s"' % column_name] + column_options
             column_name = aliases[column_name]
 
-        export.append("  %s = Column( %s )" % (column_name, ', '.join(column_options)))
+        export.append("    %s = Column(%s)" % (column_name, ', '.join(column_options)))
 
     uniques_multi = [i for i in indices['UNIQUE'] if len(i[1]) > 1]
     if len(uniques_multi):
         export.append("")
         for index_name, columns in uniques_multi:
-            export.append("  UniqueConstraint('%s', name='%s')" % ("', '".join(columns), index_name))
+            export.append("    UniqueConstraint('%s', name='%s')" % ("', '".join(columns), index_name))
 
-    export.append("")
+    if len(foreignKeys.items()):
+        export.append("")
     for column_name, v in foreignKeys.items():
         fkcol, fktable, ondelete, onupdate = v
 
@@ -173,13 +186,13 @@ def exportTable( table ):
             fkname = options[column_name].get('fkname', None)
 
         if 'norelations' in table.comment:
-            export.append('  # relationship %s ignored globally on the table' % fkname)
+            export.append('    # relationship %s ignored globally on the table' % fkname)
             continue
-        
+
         if options[column_name].get('relation', True) == 'False':
-            export.append('  # relationship %s ignored by column' % fkname)
+            export.append('    # relationship %s ignored by column' % fkname)
             continue
-        
+
         backref = ''
         if options[column_name].get('backref', True) != 'False':
             if options[column_name].get('backrefname', None) is not None:
@@ -191,27 +204,26 @@ def exportTable( table ):
             backref = ', backref="%s"' % backref
 
         column_name = aliases.get(column_name, column_name)
-        export.append('  %s = relationship( "%s", foreign_keys=[%s]%s )' % (fkname, singular(fktable), column_name, backref))
-
+        export.append('    %s = relationship("%s", foreign_keys=[%s]%s)' % (fkname, singular(fktable), column_name, backref))
 
     export.append("")
-    export.append('  def __repr__( self ):')
-    export.append('    return self.__str__()')
+    export.append('    def __repr__(self):')
+    export.append('        return self.__str__()')
 
     export.append("")
 
     # take all column you say or by default the primary ones (unless specified otherwise)
     toprint = [aliases.get(c, c) for c in [p for p, o in options.items() if o.get('toprint', str(p in indices['PRIMARY'] and options[p].get('toprint', True) != 'False')) == 'True']]
-    export.append('  def __str__( self ):')
-    export.append("    return '<"+classname+" "+ ' '.join(['%('+i+')s' for i in toprint]) +">' % self.__dict__")
+    export.append('    def __str__(self):')
+    export.append("        return '<" + classname + " " + ' '.join(['%(' + i + ')s' for i in toprint]) + ">' % self.__dict__")
 
     export.append("")
-    
+
     return export
 
-print "-"*20
+print "-" * 20
 print "-- SQLAlchemy export v%s" % version
-print "-"*20
+print "-" * 20
 
 export = []
 export.append('"""')
@@ -223,27 +235,30 @@ export.append('"""')
 export.append("")
 export.append("USE_MYSQL_TYPES = %s" % USE_MYSQL_TYPES)
 export.append("try:")
-export.append("  from . import USE_MYSQL_TYPES")
+export.append("    from . import USE_MYSQL_TYPES")
 export.append("except:")
-export.append("  pass")
+export.append("    pass")
 export.append("")
 
 tables = []
 for table in grt.root.wb.doc.physicalModels[0].catalog.schemata[0].tables:
     print " -> Working on %s" % table.name
-    tables.extend( exportTable(table) )
+    tables.extend(exportTable(table))
 
 export.append("")
 export.append("from sqlalchemy.orm import relationship")
 export.append("from sqlalchemy import Column, ForeignKey")
 export.append("from sqlalchemy.schema import UniqueConstraint")
 export.append("from sqlalchemy.ext.declarative import declarative_base")
-if len(types['sqla']): export.append("from sqlalchemy import %s" % ', '.join(types['sqla']))
+if len(types['sqla']):
+    export.append("from sqlalchemy import %s" % ', '.join(types['sqla']))
 export.append("")
 export.append("if USE_MYSQL_TYPES:")
-if len(types['mysql']): export.append("  from sqlalchemy.dialects.mysql import %s" % ', '.join(types['mysql']))
+if len(types['mysql']):
+    export.append("    from sqlalchemy.dialects.mysql import %s" % ', '.join(types['mysql']))
 export.append("else:")
-if len(types['sqla_alt']): export.append("  from sqlalchemy import %s" % ', '.join(types['sqla_alt']))
+if len(types['sqla_alt']):
+    export.append("    from sqlalchemy import %s" % ', '.join(types['sqla_alt']))
 export.append("")
 export.append("Base = declarative_base()")
 export.append("")
@@ -253,4 +268,3 @@ export.extend(tables)
 
 grt.modules.Workbench.copyToClipboard('\n'.join(export))
 print "Copied to clipboard"
-
